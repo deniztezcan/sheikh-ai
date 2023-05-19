c667a293767025423713fd3b92d437f140156311
Deniz Tezcan <10155092+deniztezcan@users.noreply.github.com>
2023-05-19 10:51:01 +0200

    yes

--- packages/Engine/Engine.php
+++ packages/Engine/Engine.php
@@ -2,15 +2,76 @@
 
 namespace Packages\Engine;
 
+use App\Models\Ayah;
+use App\Models\Hadith;
+use MathPHP\Statistics\Distance;
 use Orhanerday\OpenAi\OpenAi;
+use Yethee\Tiktoken\EncoderProvider;
 
 class Engine
 {
     public $open_ai;
 
+    public $encoder;
+
     public function __construct()
     {
         $this->open_ai = new OpenAi(env('OPENAI_API_KEY'));
+        $this->encoder = new EncoderProvider();
+    }
+
+    private function num_tokens(
+        string $text,
+        string $model
+    ): int {
+        $encoder = $this->encoder->getForModel($model);
+        $tokens = $encoder->encode($text);
+
+        return count($tokens);
+    }
+
+    private function lookForRelatedNess(
+        $response,
+        $obj
+    ) {
+        $relatedness = Distance::consine($response['data'][0]['embedding'], $obj['embedding']);
+
+        return [$obj['text'], $relatedness];
+    }
+
+    private function stringsByRelatedness(
+        string $string,
+        string $model,
+        int $top = 100
+    ) {
+        $response = $this->open_ai->embeddings([
+            'model' => $model,
+            'input' => $string,
+        ]);
+
+        $strings_and_relatednesses = [];
+        foreach (Ayah::all() as $i => $row) {
+            $strings_and_relatednesses[] = $this->lookForRelatedNess($response, $row);
+        }
+        foreach (Hadith::all() as $i => $row) {
+            $strings_and_relatednesses[] = $this->lookForRelatedNess($response, $row);
+        }
+
+        usort($strings_and_relatednesses, function ($a, $b) {
+            return $b[1] <=> $a[1];
+        });
+
+        $strings = [];
+        $relatednesses = [];
+        foreach ($strings_and_relatednesses as $item) {
+            $strings[] = $item[0];
+            $relatednesses[] = $item[1];
+        }
+
+        $strings = array_slice($strings, 0, $top);
+        $relatednesses = array_slice($relatednesses, 0, $top);
+
+        return [$strings, $relatednesses];
     }
 
     private function queryMessage(
